a33b6d50c3e7ff0b0d2e77be90da069e3299b310
Prateekkumar <prateekkumar.d@thewholewave.com>
2023-10-26 09:37:08 +0530

    project1

--- testingproject/routes/web.php
+++ testingproject/routes/web.php
@@ -15,3 +15,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('user', 'SharkController');
 Route::get('test', 'SharkController@testfunc');
+
+
+Route::get('routes', function () {
+    $routeCollection = Route::getRoutes();
+
+    echo "<table style='width:100%'>";
+    echo "<tr>";
+    echo "<td width='10%'><h4>HTTP Method</h4></td>";
+    echo "<td width='10%'><h4>Route</h4></td>";
+    echo "<td width='10%'><h4>Name</h4></td>";
+    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
+    echo "</tr>";
+    foreach ($routeCollection as $value) {
+        echo "<tr>";
+        echo "<td>" . $value->methods()[0] . "</td>";
+        echo "<td><a href='".$value->uri()."'>" . $value->uri() . "</a></td>";
+        echo "<td>" . $value->getName() . "</td>";
+        echo "<td>" . $value->getActionName() . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+});
